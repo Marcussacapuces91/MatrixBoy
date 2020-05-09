c8406ad2410525786759a88f177e288554037331
Marc Sibert <marc@sibert.fr>
2020-05-09 23:04:15 +0200

    Jeu Snake

--- MatrixBoy/snake.h
+++ MatrixBoy/snake.h
@@ -30,18 +30,8 @@ public:
  * @return un booléen qui indique le bon déroulement de la méthode.
  */
   boolean setup() {
-    matrix.println(F("Snake (c) 2020, MSoft"));
-    delay(1000);
-    for (byte i = 3; i > 0; --i) {
-      matrix.clear(true);
-      delay(50);
-      matrix.clear();
-      matrix.println('0' + i);
-      delay(1000);
-    }
-    matrix.clear(true);
-    delay(50);
-    matrix.clear();
+
+    srand(micros());
     
     return true;
   }
@@ -52,120 +42,145 @@ public:
  *         En cas de retour FALSE, il ne faut plus appeler cette méthode (fin du jeu par exemple).
  */
   boolean loop() {
-    static unsigned pt = 0;
-    static int8_t dx = -1;
-    static int8_t dy = 0;
-    static auto t = millis();
-    static auto p = millis();
-    static byte comptePomme = 0;
-    static unsigned long delayTone = 0; // OFF
+    unsigned tot = 0;
 
-    const auto b = matrix.button();
-    if ((b & Matrix::UP) && !(b & Matrix::DOWN)) { dx = 0; dy = +1; }
-    else if (!(b & Matrix::UP) && (b & Matrix::DOWN)) { dx = 0; dy = -1; };
-    if ((b & Matrix::LEFT) && !(b & Matrix::RIGHT)) { dx = -1; dy = 0; }
-    else if (!(b & Matrix::LEFT) && (b & Matrix::RIGHT)) { dx = +1; dy = 0; };
-    
-    const auto m = millis();
+    for (byte p = 3; p > 0; --p) {
+      while (!matrix.pressA(String(p) + String(F(" snake")) + ( p > 1 ? String('s') : String() ) + String(F(" - [A] start... ")))) ;
 
-    if ( delayTone && ((m - delayTone) > 50) ) {
-      noTone(Matrix::TONE_PIN);
-      delayTone = 0;
+      const auto pts = partie(500);
+
+      tone(Matrix::TONE_PIN, NOTE_C4, 250);
+      delay(350);
+      tone(Matrix::TONE_PIN, NOTE_B3, 250);
+      delay(350);
+      tone(Matrix::TONE_PIN, NOTE_A3, 500);
+      matrix.clear(true);
+      delay(75);
+      matrix.clear();
+        
+      while (!matrix.pressA( String(4-p) + String(F(". ")) + String(pts) + String(F(" pts. [A] play again... ")) )) ;
+      tot += pts;
     }
+    while (!matrix.pressA( String(F("Total ")) + String(tot) + String(F(" pts. [A] End... ")) )) ;
 
-    if ((m - p > 75) && pomme.set) {
-      p = m;
-      static bool light = false;
+    return false;
+  }
 
-      if (light) {
-        matrix.set(pomme.x, pomme.y);
-      } else {
-        matrix.unSet(pomme.x, pomme.y);
+protected:
+/**
+ * Play a set and return the result.
+ * @param speed delay (ms) between each movement.
+ * @return points value.
+ */
+  unsigned partie(const unsigned speed = 500) {
+    unsigned pts = 0;
+    int8_t dx = -1;
+    int8_t dy = 0;
+
+    auto delayPomme = millis();
+    auto delayMove = millis();
+    unsigned long delayTone = 0; // OFF
+
+    bool light = false;
+  
+    serpent.last = 0;
+    serpent.headX = 4;
+    serpent.headY = 4;
+    
+    pomme.set = false;
+
+    while (true) {
+
+// Test buttons & change direction
+      const auto b = matrix.button();
+      if ((b & Matrix::UP) && !(b & Matrix::DOWN)) { dx = 0; dy = +1; }
+      else if (!(b & Matrix::UP) && (b & Matrix::DOWN)) { dx = 0; dy = -1; };
+      if ((b & Matrix::LEFT) && !(b & Matrix::RIGHT)) { dx = -1; dy = 0; }
+      else if (!(b & Matrix::LEFT) && (b & Matrix::RIGHT)) { dx = +1; dy = 0; };
+    
+// Current time
+      const auto ct = millis();
+
+// Stop sound after 50ms
+      if ( delayTone && ((ct - delayTone) > 50) ) {
+        noTone(Matrix::TONE_PIN);
+        delayTone = 0;
       }
-      light = !light;
-      
-    }
-    
-    if (m - t > 500) { // Every 500 ms
-      t = m;
 
-// Effacer serpent
-      auto x = serpent.headX;
-      auto y = serpent.headY;
-      matrix.unSet(x, y);
-      for (auto i = 0; i < serpent.len(); ++i) {
-        serpent.diff(i, x, y);
+// Blink Apple, if set all 100 ms
+      if ((ct - delayPomme > 100) && pomme.set) {
+        delayPomme = ct;
+        if (light) {
+          matrix.set(pomme.x, pomme.y);
+        } else {
+          matrix.unSet(pomme.x, pomme.y);
+        }
+        light = !light;
+      }
+
+// Every "speed" ms move Snake    
+      if (ct - delayMove > speed) {
+        delayMove = ct;
+
+  // Clear existing snake
+  ///@todo à priori juste le dernier pixel à supprimer !!!
+        byte x = serpent.headX;
+        byte y = serpent.headY;
+/*
         matrix.unSet(x, y);
-      }
-      
-// Déplacer serpent
-      serpent.move(dx, dy);
-
-// Afficher serpent      
-      x = serpent.headX;
-      y = serpent.headY;
-      matrix.set(x, y);
-      for (auto i = 0; i < serpent.len(); ++i) {
-        serpent.diff(i, x, y);
+        for (byte i = 0; i < serpent.len(); ++i) {
+          serpent.diff(i, x, y);
+          matrix.unSet(x, y);
+        }
+*/       
+        for (byte i = 0; i < serpent.len(); ++i) {
+          serpent.diff(i, x, y);
+        }
+        matrix.unSet(x, y);
+        
+  // Move snake
+        serpent.move(dx, dy);
+  
+  // Display snake
+  ///@todo rajouter la tête & vérifier s'il faut finalement prolonger la queue
+        x = serpent.headX;
+        y = serpent.headY;
         matrix.set(x, y);
-        if (serpent.headX == x && serpent.headY == y) { // croisement
-          tone(Matrix::TONE_PIN, NOTE_C4, 250);
-          delay(350);
-          tone(Matrix::TONE_PIN, NOTE_B3, 250);
-          delay(350);
-          tone(Matrix::TONE_PIN, NOTE_A3, 500);
-
-          String s(pt);
-          s += String(F(" pts"));
-          matrix.println(s);
-          delay(3000);
-          matrix.clear(true);
-          delay(50);
-          matrix.clear();
-    
-          serpent.last = 0;
-          serpent.headX = 4;
-          serpent.headY = 4;
-          dx = -1;
-          dy = 0;
-          
-          comptePomme = 0;
-          pomme.set = false;
-          
+        for (byte i = 0; i < serpent.len(); ++i) {
+          serpent.diff(i, x, y);
+          matrix.set(x, y);
+          if (serpent.headX == x && serpent.headY == y) { // body crossing
+            return pts;
+          }
+        }
+  
+        if (pomme.set) {
+  // Tête serpent sur la pomme
+          if ( (serpent.headX == pomme.x) && (serpent.headY == pomme.y) ) {
+            tone(Matrix::TONE_PIN, NOTE_B0);
+            delayTone = ct;
+            pomme.set = false;
+            ++serpent.last;
+            ++pts;
+          }
+        } else {
+          pomme.set = true;
+          do {
+            pomme.x = rand() % 8;
+            pomme.y = rand() % 8;
+          } while (matrix.test(pomme.x, pomme.y));
         }
       }
 
-// Tête serpent sur la pomme
-      if ( pomme.set && (serpent.headX == pomme.x) && (serpent.headY == pomme.y) ) {
-        tone(Matrix::TONE_PIN, NOTE_B0);
-        delayTone = m;
-        comptePomme = 0;  // reset;
-        pomme.set = false;
-        ++serpent.last;
-        ++pt;
-      }
-      
-      ++comptePomme;
-      if (!(comptePomme % 2) && !pomme.set) { // ne pas faire réaparaitre la pomme immédiatement
-        pomme.set = true;
-        do {
-          pomme.x = rand() % 8;
-          pomme.y = rand() % 8;
-        } while (matrix.test(pomme.x, pomme.y));
-//        matrix.set(pomme.x, pomme.y);
-      }
-      
-    }
-
-    
-    
-    return true;
+      yield();
+    }    
   }
 
 private:
-/// Une référence vers la matrice de Leds
+/// Une référence vers la matrice de Leds.
   Matrix& matrix;
 
+/// Structure et variable du serpent.
   struct {
     byte headX:3;
     byte headY:3;
@@ -201,12 +216,13 @@ private:
       // else if (dx == -1) data[0] |= 0x0;
     }
     
-  } serpent = { 4, 4, 0, { 0, 0, 0, 0 } };
+  } serpent; //  = { 4, 4, 0, { 0, 0, 0, 0 } };
 
+/// Structure et variable de la pomme.
   struct {
     byte x:3;
     byte y:3;
     bool set:1;
-  } pomme = {0, 0, false};
+  } pomme; // = {0, 0, false};
   
 };
